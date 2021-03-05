be015198e8d02796ed11f5049f2535e54e0dc968
Navdeep92 <balgurleen77@gmail.com>
2021-03-05 19:24:07 +0530

    Constructor commit

--- src/GurleenKaur_C0789762_A5_A6/Assignment_A5_A6.java
+++ src/GurleenKaur_C0789762_A5_A6/Assignment_A5_A6.java
@@ -1,5 +1,25 @@
 package GurleenKaur_C0789762_A5_A6;
 
+import javax.swing.*;
+
 public class Assignment_A5_A6 {
+<<<<<<< HEAD
+    double serviceCostPackageA;
+    double serviceCostPackageB;
+    double opCostStorageSmall;
+    double opCostStorageLarge;
+    double itemBoxesSmall;
+    double itemBoxesLarge;
+
+    public Assignment_A5_A6(double serviceCostPackageA, double serviceCostPackageB,
+                            double opCostStorageSmall, double opCostStorageLarge,
+                            double itemBoxesSmall, double itemBoxesLarge) {
+        this.serviceCostPackageA = serviceCostPackageA;
+        this.serviceCostPackageB = serviceCostPackageB;
+        this.opCostStorageSmall = opCostStorageSmall;
+        this.opCostStorageLarge = opCostStorageLarge;
+        this.itemBoxesSmall = itemBoxesSmall;
+        this.itemBoxesLarge = itemBoxesLarge;
+    }
 
 }
